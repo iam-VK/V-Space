ff897ca2c81c5d8462bf160e77b235cd5a52fa20
VK <vishwakumar.petit@gmail.com>
2024-09-12 18:25:46 +0530

    first commit

---
+++ run_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+run_microservice() {
+  local microservice_dir=$1
+  (cd "$microservice_dir" && ./run.sh)
+}
+
+run_microservice "frontend" &
+run_microservice "search" &
+run_microservice "frame_extractor" &
+run_microservice "media_server" &
+run_microservice "Vision_Transformer" &
+
+wait
